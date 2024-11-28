fe2efe6e9cecfabaa3562a8d7635542b06ed36b1
Oswaldo Rodriguez <rodriosw13@gmail.com>
2024-11-28 10:44:55 -0400

    Código js

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log("hola");
+const form = document.querySelector('form');
+const pwd = document.getElementById('password').value;
+const pwdConfirm = document.getElementById('password-confirm').value;
+const mensaje = document.querySelector('.error-message');
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', function(e) {
+ console.log(pwd);
+ 
+})
+
