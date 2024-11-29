2cd0b6808e88f4262644f582e037e45071c88a37
Oswaldo Rodriguez <rodriosw13@gmail.com>
2024-11-29 09:58:17 -0400

    Terminando el ejercicio

--- main.js
+++ main.js
@@ -1,11 +1,29 @@
+const expresion = {
+  password: /^.{4,12}$/ //contraseña de 4 a 12 caracteres.
+}
+
+
 const form = document.querySelector('form');
-const pwd = document.getElementById('password').value;
-const pwdConfirm = document.getElementById('password-confirm').value;
+const pwd = document.getElementById('password');
+const pwdConfirm = document.getElementById('password-confirm');
 const mensaje = document.querySelector('.error-message');
 const btn = document.querySelector('button');
 
-btn.addEventListener('click', function(e) {
- console.log(pwd);
- 
-})
 
+
+btn.addEventListener('click', function(e) {
+  e.preventDefault();
+  if(!expresion.password.test(pwd.value)){
+    mensaje.textContent = 'La contraseña debe tener entre 4 y 12 caracteres';
+    pwd.classList.add('input-error');
+    pwdConfirm.classList.add('input-error');
+  }
+
+  if(pwd.value !== pwdConfirm.value) {
+    pwdConfirm.classList.add('input-error');
+    pwd.classList.add('input-error');
+    mensaje.textContent = 'Las contraseñas no coinciden.';
+  }
+
+  console.log('Formulario enviado.')
+})
